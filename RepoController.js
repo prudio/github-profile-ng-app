38ff50c9e3cdce58491ea6855d89d6cc1849b2f0
prudio <prudio@uw.edu>
2015-01-25 12:52:52 -0800

    initial commit

---
+++ RepoController.js
@@ -0,0 +1,24 @@
+(function() {
+  
+  var module = angular.module("githubViewer");
+  
+  var RepoController = function($scope, github, $routeParams) {
+    
+    var onRepo = function(data) {
+      $scope.repo = data;
+    };
+    
+    var onError = function(reason) {
+      $scope.error = reason;
+    };
+    
+    var reponame = $routeParams.reponame;
+    var username = $routeParams.username;
+    
+    github.getRepoDetails(username, reponame)
+          .then(onRepo, onError);
+  };
+
+  module.controller("RepoController", RepoController);
+  
+}());
